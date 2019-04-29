48dce1c9f74f4b35fe2b2499cc0de498d2933e02
mads østermark <madscramt@gmail.com>
2019-04-29 14:25:30 +0200

    added A VERY IMPORTANT try catch

--- app/src/main/java/autospot/autospot/autospot/AutoSpotRotationHandler.kt
+++ app/src/main/java/autospot/autospot/autospot/AutoSpotRotationHandler.kt
@@ -5,6 +5,7 @@ import java.lang.Math.atan
 import java.net.DatagramPacket
 import java.net.DatagramSocket
 import java.net.InetAddress
+import kotlin.math.roundToInt
 
 class AutoSpotRotationHandler {
     var x: Double = 0.0
@@ -15,31 +16,35 @@ class AutoSpotRotationHandler {
         val RAD_TO_STEPS = 25.78310078
         val spotPos = Vector2(x, y)
         val dir = pos.sub(spotPos)
-        val xySteps = atan(dir.y / dir.x) * RAD_TO_STEPS
+        val xySteps = (atan(dir.y / dir.x) * RAD_TO_STEPS).roundToInt()
 
-        val yzSteps = atan(pos.length(spotPos) / y) * RAD_TO_STEPS
+        val yzSteps = (90 - (atan(pos.length(spotPos) / y) * RAD_TO_STEPS)).roundToInt()
+        sendUDPPacket(xySteps.toString() + "," + yzSteps.toString())
+    }
+
+    fun sendUDPPacket(str: String) {
+        var address: InetAddress? = null
+        try{
+            address = InetAddress.getByName(ip)
+        }
+        catch (e: Error){
+
+        }
+        if (ip != "" && address != null) {
+            try {
+                val bytes = str.toByteArray()
+                val socket = DatagramSocket()
+                val packet = DatagramPacket(bytes, bytes.size, address, 69)
+                socket.broadcast = true
+                socket.send(packet)
+                socket.close()
+            } catch (err: Error) {
+                Log.v("err", err.message)
+            }
+        }
     }
 
     init {
-        Thread {
-            while(true){
-                Log.v("ip", ip)
-                if(ip != ""){
-                    try {
-                        val input = "ding dong"
-                        val bytes = input.toByteArray()
-                        val socket = DatagramSocket()
-                        val packet = DatagramPacket(bytes, bytes.size, InetAddress.getByName(ip), 69)
-                        socket.broadcast = true
-                        socket.send(packet)
-                        socket.close()
-                    }
-                    catch (err: Error){
-                        Log.v("err", err.message)
-                    }
-                }
-                Thread.sleep(1000)
-            }
-        }.start()
+
     }
 }
